2603577be3cea863950bb23874450e9c0c686ab1
Sumaiya-topu <topusumaiya@gmail.com>
2023-06-11 01:45:52 +0600

    databse connected successfully

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const dotenv = require("dotenv").config();
+mongoose.set("strictQuery", false);
+
 const express = require("express");
-const app = require("./app");
-// const mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
 const cors = require("cors");
+const app = require("./app");
+
+// database connection
+mongoose
+  .connect(process.env.DATABASE_URI, { useNewUrlParser: true })
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log(err));
+
+//use  middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.DATABASE_URI;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`go-tutor server is running on port ${port}`);
