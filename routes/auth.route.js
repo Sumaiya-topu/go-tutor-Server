c32450c01e3e385d08493a24151b8f8199b5fad5
Sumaiya-topu <topusumaiya@gmail.com>
2023-06-16 15:51:17 +0600

    auth route added

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controller/user.controller");
 // const verifyToken = require();
-router.get("/", userController.getUsers);
+router.post("/signup", userController.signUp);
 module.exports = router;
